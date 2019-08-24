56be9e68f3293d7d2ac2fcee5cb55e86e76b5868
xyz <xyzstyle@163.com>
2019-08-24 09:20:47 +0800

    V3:    主要完成了ShowNoteActivity和EditNoteActivity之间跳转

--- app/src/main/java/xyz/note/EditNoteActivity.java
+++ app/src/main/java/xyz/note/EditNoteActivity.java
@@ -1,11 +1,15 @@
 package xyz.note;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 /**
@@ -14,21 +18,52 @@ import android.view.MenuItem;
  */
 public class EditNoteActivity extends AppCompatActivity {
 
+    private EditText mNameEdt;
+    private EditText mContentEdt;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+
         setContentView(R.layout.activity_edit_note);
+        findViews();
+        if (intent != null) {
+            mNameEdt.setText(intent.getStringExtra("name"));
+            mContentEdt.setText(intent.getStringExtra("content"));
+        }
         Toolbar toolbar = findViewById(R.id.toolbar_edit_note);
         toolbar.inflateMenu(R.menu.toolbar_edit_note);
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem menuItem) {
-                if (menuItem.getItemId() == R.id.action_menu_cancel_edit) {
-                    EditNoteActivity.this.finish();
+
+                switch (menuItem.getItemId()) {
+                    case R.id.action_menu_cancel_edit:
+                        EditNoteActivity.this.finish();
+                        break;
+                    case R.id.action_menu_save_note:
+                        break;
+
+                    default:
                 }
+
 
                 return false;
             }
         });
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(EditNoteActivity.this,"back",Toast.LENGTH_LONG).show();
+                EditNoteActivity.this.finish();
+            }
+        });
+    }
+
+    private void findViews() {
+        mNameEdt = findViewById(R.id.edt_note_name);
+        mContentEdt = findViewById(R.id.edt_note_content);
     }
 }
+
--- app/src/main/java/xyz/note/ShowNoteActivity.java
+++ app/src/main/java/xyz/note/ShowNoteActivity.java
@@ -1,19 +1,50 @@
 package xyz.note;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
 /**
  * Created by xyz on 2019/8/22.
  * Project Name:ZNote2020
  */
 public class ShowNoteActivity extends AppCompatActivity {
+
+
+
+    private TextView mNoteNameTv;
+    private TextView mNoteContentTv;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_note);
+        findViews();
         Toolbar toolbar = findViewById(R.id.toolbar_show_note);
         toolbar.inflateMenu(R.menu.toolbar_show_note);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.action_edit_note:
+                        Intent intent=new Intent(ShowNoteActivity.this,EditNoteActivity.class);
+                        intent.putExtra("name", mNoteNameTv.getText().toString());
+                        intent.putExtra("content", mNoteContentTv.getText().toString());
+                        startActivity(intent);
+                }
+
+                return false;
+            }
+        });
+    }
+
+    private void findViews() {
+        mNoteNameTv = findViewById(R.id.tv_note_name);
+        mNoteContentTv = findViewById(R.id.tv_note_content);
     }
 }
